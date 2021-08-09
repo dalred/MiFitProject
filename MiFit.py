9dd4eaefc5e56c4142ce7e269251ae063ac31453
dalred <RcrF63gGG7HpbAT>
2021-08-09 23:07:26 +0300

    Версия 1.5.1

--- MiFit.py
+++ MiFit.py
@@ -34,7 +34,6 @@ def copy_pasta(path_to):
     ws.format('E2:E', {'numberFormat': {'type': 'PERCENT','pattern': '00.00%'}})
     ws.format('A2:E',{"horizontalAlignment": 'CENTER','textFormat': {'fontSize': '10'}})
 
-
 def path_to_csv():
     filtered_csv = []
     for root, dirs, files in os.walk(folder_url):
@@ -46,24 +45,27 @@ def path_to_csv():
                 filtered_csv.append(path)
     return filtered_csv
 
-if len(path_to_csv()) > 0:
-    for i in path_to_csv():
-        print('Записываю ' + i)
-        copy_pasta(i)
-        path = i.split("/")
-        path_join = '/'.join([path[0], path[1], path[2]])
-        path_zip = path_join + '.zip'
-        if os.path.isfile(path_zip):
-            print('Удаляю zip: '+path_zip)
-            os.remove(path_zip)
-        if len(path) == 3:
-            # Проверяем если это файл
-            if os.path.isfile(path_join):
+def main_():
+    if len(path_to_csv()) > 0:
+        for i in path_to_csv():
+            print('Записываю ' + i)
+            copy_pasta(i)
+            path = i.split("/")
+            path_join = '/'.join([path[0], path[1], path[2]])
+            path_zip = path_join + '.zip'
+            if os.path.isfile(path_zip):
+                print('Удаляю zip: '+path_zip)
+                os.remove(path_zip)
+            if len(path) == 3:
+                # Проверяем если это файл
+                if os.path.isfile(path_join):
+                    print('Удаляю: ' + path_join)
+                    os.remove(path_join)
+            else:
+                # Если это папка
                 print('Удаляю: ' + path_join)
-                os.remove(path_join)
-        else:
-            # Если это папка
-            print('Удаляю: ' + path_join)
-            shutil.rmtree(path_join)
-else:
-    print("Файлы отсутствуют!")
+                shutil.rmtree(path_join)
+    else:
+        print("Файлы отсутствуют!")
+
+main_()
