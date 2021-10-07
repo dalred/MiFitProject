2b2cc16064fa3f6794f5b9887661bcefd471c29d
dalred <RcrF63gGG7HpbAT>
2021-10-08 01:47:14 +0300

    Версия 1.6.1

--- MiFit.py
+++ MiFit.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-import time,pandas,os, re,shutil
+import time, pandas, os, re, shutil
 import gspread
-from gspread_dataframe import get_as_dataframe, set_with_dataframe
+from gspread_dataframe import set_with_dataframe
 
 folder_url = "D:\Downloads"
 regex_csv = 'BODY_.*\.csv$'
 SCOPES = ['https://www.googleapis.com/auth/drive']
-
 
 gc = gspread.service_account(filename='C:/Users/dalre/PycharmProjects/csvproject/pythontableproject.json')
 sht1 = gc.open_by_key('')
@@ -14,27 +13,33 @@ sht1 = gc.open_by_key('')
 
 def copy_pasta(path_to):
     list_ = ['timestamp', 'weight', 'fatRate', 'muscleRate']
+
     def date_parser(string_list):
         return [time.ctime(float(x)) for x in string_list]
-    #Cчитываем csv
+
+    # Cчитываем csv
     data = pandas.read_csv(path_to, parse_dates=[0], sep=',', date_parser=date_parser)[list_]
     fatRate = data['fatRate'] > 0
     muscleRate = data['muscleRate'] > 0
     # Добавляем еще колонку (Column)
-    data['Muscle percentage'] = data['muscleRate']/data['weight']
-
-    #Берем только те значения кто больше 0, на всякий случай.
+    data['Muscle percentage'] = data['muscleRate'] / data['weight']
+    data['fatRate'] = data['fatRate'] / 100
+    # Берем только те значения кто больше 0, на всякий случай.
     data = data[fatRate & muscleRate]
+    data = data.sort_values('timestamp')  # В CSV почему-то стало наоборот по убыванию
     ws = sht1.worksheet('Weight_Musclerate')
     max_rows = len(ws.get_all_values())
-    set_with_dataframe(ws, data, row=max_rows+1, col=1, include_column_header=False)
+    set_with_dataframe(ws, data, row=max_rows + 1, col=1, include_column_header=False)
 
-    ws.format('A2:A', {'numberFormat': {'type': 'DATE','pattern': 'dd.MM.yyyy'}})
-    ws.format('B2:D', {'numberFormat': {'type': 'NUMBER','pattern': '0.00'}})
-    ws.format('E2:E', {'numberFormat': {'type': 'PERCENT','pattern': '00.00%'}})
-    ws.format('A2:E',{"horizontalAlignment": 'CENTER','textFormat': {'fontSize': '10'}})
+    ws.format('A2:A', {'numberFormat': {'type': 'DATE', 'pattern': 'dd.MM.yyyy'}})
+    ws.format('B2:B', {'numberFormat': {'type': 'NUMBER', 'pattern': '0.00'}})
+    ws.format('C2:C', {'numberFormat': {'type': 'PERCENT', 'pattern': '00.00%'}})
+    ws.format('D2:D', {'numberFormat': {'type': 'NUMBER', 'pattern': '0.00'}})
+    ws.format('E2:E', {'numberFormat': {'type': 'PERCENT', 'pattern': '00.00%'}})
+    ws.format('A2:E', {"horizontalAlignment": 'CENTER', 'textFormat': {'fontSize': '10'}})
 
-def path_to_csv(folder_url,regex_csv):
+
+def path_to_csv(folder_url, regex_csv):
     filtered_csv = []
     for root, dirs, files in os.walk(folder_url):
         # del dirs[:]  # go only one level deep
@@ -46,15 +51,15 @@ def path_to_csv(folder_url,regex_csv):
     return filtered_csv
 
 
-if len(path_to_csv(folder_url,regex_csv)) > 0:
-    for i in path_to_csv(folder_url,regex_csv):
+if len(path_to_csv(folder_url, regex_csv)) > 0:
+    for i in path_to_csv(folder_url, regex_csv):
         print('Записываю ' + i)
         copy_pasta(i)
         path = i.split("/")
         path_join = '/'.join([path[0], path[1], path[2]])
         path_zip = path_join + '.zip'
         if os.path.isfile(path_zip):
-            print('Удаляю zip: '+path_zip)
+            print('Удаляю zip: ' + path_zip)
             os.remove(path_zip)
         if len(path) == 3:
             # Проверяем если это файл
@@ -67,4 +72,3 @@ if len(path_to_csv(folder_url,regex_csv)) > 0:
             shutil.rmtree(path_join)
 else:
     print("Файлы отсутствуют!")
-
--- MiFit_activity.py
+++ MiFit_activity.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 import pandas,os, re,shutil
 import gspread
-from gspread_dataframe import set_with_dataframe
+from gspread_dataframe import set_with_dataframe,get_as_dataframe
 
 
 folder_url = "D:\Downloads"
 regex_csv = 'ACTIVITY_(?!STAGE|MINUTE).*\.csv$'
 SCOPES = ['https://www.googleapis.com/auth/drive']
-
 
 gc = gspread.service_account(filename='C:/Users/dalre/PycharmProjects/csvproject/pythontableproject.json')
 sht1 = gc.open_by_key('')
@@ -23,20 +22,67 @@ def path_to_csv(folder_url, regex_csv):
                 filtered_csv.append(path)
     return filtered_csv
 
+def avg_weeks():
+    print('Cчитываю старый Dataframe c листа!')
+    df_old = get_as_dataframe(ws, parse_dates=True, usecols=[0,1,2])
+    df_old = df_old[list_].dropna(how='all')
+    df_old['dateAvg'] = pandas.to_datetime(df_old['date'], format="%d.%m.%Y")
+    df_old = df_old.set_index('dateAvg')
+    df_old = df_old.resample('W').mean()
+    #print(df)
+    set_with_dataframe(ws, df_old, row=2, col=4, include_column_header=False,include_index=True)
+
 def copy_pasta(path_to):
-    list_ = ['date', 'steps', 'calories']
     data = pandas.read_csv(path_to, parse_dates=[0], sep=',')[list_]
     #Новый dataframe
-    ws = sht1.worksheet('Activity')
     max_rows = len(ws.get_all_values())
+    next_row = max_rows + 1
+    end_row = next_row+len(data)
     set_with_dataframe(ws, data, row=max_rows+1, col=1, include_column_header=False)
-    ws.format('A2:A', {'numberFormat': {'type': 'DATE','pattern': 'dd.MM.yyyy'}})
-    ws.format('B2:E', {'numberFormat': {'type': 'NUMBER','pattern': '0'}})
+    ws.format('A2:A',
+              {'numberFormat':
+                   {'type': 'DATE', 'pattern': 'dd.MM.yyyy'},
+               })
+    ws.format('D2:D',
+              {'numberFormat':
+                   {'type': 'DATE', 'pattern': 'dd.MM.yyyy'},
+               })
+    ws.format('B2:E',
+              {'numberFormat':
+                   {'type': 'NUMBER', 'pattern': '0'}
+               })
+    ws.format('A' + str(next_row) + ':C' + str(end_row),
+              {
+                  'horizontalAlignment': 'CENTER',
+                  'textFormat': {'fontSize': '9'},
+                  'verticalAlignment': 'MIDDLE',
+                  'wrapStrategy': 'WRAP'
+              })
+
+def format_table_two():
+    ws.format('D2:D',
+              {'numberFormat':
+                   {'type': 'DATE', 'pattern': 'dd.MM.yyyy'},
+               })
+    ws.format('E2:F',
+              {'numberFormat':
+                   {'type': 'NUMBER', 'pattern': '0'}
+               })
+    ws.format('D2:F',
+              {
+                  'horizontalAlignment': 'CENTER',
+                  'textFormat': {'fontSize': '9'},
+                  'verticalAlignment': 'MIDDLE',
+                  'wrapStrategy': 'WRAP'
+              })
+
 
 if len(path_to_csv(folder_url,regex_csv)) > 0:
     for i in path_to_csv(folder_url,regex_csv):
         print('Записываю ' + i)
         copy_pasta(i)
+        avg_weeks()
+        format_table_two()
         path = i.split("/")
         path_join = '/'.join([path[0], path[1], path[2]])
         path_zip = path_join + '.zip'
